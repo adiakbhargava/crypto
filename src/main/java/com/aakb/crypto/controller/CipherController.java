ce2f222fada824229ee6addf8e5bfe3037dfa93d
Aditya Bhargava <adiakbhargava@gmail.com>
2024-09-02 13:26:40 -0500

    added asymm encryption and decryption URIs

--- src/main/java/com/aakb/crypto/controller/CipherController.java
+++ src/main/java/com/aakb/crypto/controller/CipherController.java
@@ -1,6 +1,7 @@
 package com.aakb.crypto.controller;
 
 import com.aakb.crypto.domain.*;
+import com.aakb.crypto.impl.AsymmEncDec;
 import com.aakb.crypto.impl.SymmEncDec;
 import com.aakb.crypto.impl.Utils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,19 +12,15 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
 @RestController
 class CipherController {
-    CipherController() {
-    }
-
     @PostMapping("/symm-enc")
     CipherResponse symmetricEncryption(@RequestBody CipherRequest cipherRequest) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        SecretKey hydratedKey = Utils.hydrateKeyFromBase64(cipherRequest.getSecretKey(), cipherRequest.getSecretKeyAlgoName());
+        SecretKey hydratedKey = Utils.hydrateKeyFromBase64(cipherRequest.getKey(), cipherRequest.getKeyAlgoName());
         byte[] defaultIV = Utils.createDefaultIv();
         byte[] cipherBytes = SymmEncDec.encrypt(cipherRequest.getPlainText(), hydratedKey, cipherRequest.getCipherAlgoName(), defaultIV);
         return CipherResponse.builder()
@@ -34,11 +31,31 @@ class CipherController {
 
     @PostMapping("/symm-dec")
     DecipherResponse symmetricDecryption(@RequestBody DecipherRequest decipherRequest) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        SecretKey hydratedKey = Utils.hydrateKeyFromBase64(decipherRequest.getSecretKey(), decipherRequest.getSecretKeyAlgoName());
+        SecretKey hydratedKey = Utils.hydrateKeyFromBase64(decipherRequest.getKey(), decipherRequest.getKeyAlgoName());
         byte[] decodedIV = Base64.getDecoder().decode(decipherRequest.getIV());
         String decipherStr = SymmEncDec.decrypt(decipherRequest.getCipherText(), hydratedKey, decipherRequest.getCipherAlgoName(), decodedIV);
         return DecipherResponse.builder()
                 .decipherText(decipherStr)
+                .build();
+    }
+
+    @PostMapping("/asymm-enc")
+    AsymmCipherResponse asymmetricEncryption(@RequestBody CipherRequest cipherRequest) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
+        PrivateKey privKey = Utils.hydratePrivKey(cipherRequest.getKey(), cipherRequest.getKeyAlgoName());
+        byte[] cipherBytes = AsymmEncDec.encrypt(cipherRequest.getPlainText(), privKey, cipherRequest.getCipherAlgoName());
+
+        return AsymmCipherResponse.builder()
+                .cipherText(Base64.getEncoder().encodeToString(cipherBytes))
+                .build();
+    }
+
+    @PostMapping("/asymm-dec")
+    DecipherResponse asymmetricDecryption(@RequestBody DecipherRequest decipherRequest) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
+        PublicKey pubKey = Utils.hydratePubKey(decipherRequest.getKey(), decipherRequest.getKeyAlgoName());
+        String decipheredStr = AsymmEncDec.decrypt(decipherRequest.getCipherText(), pubKey, decipherRequest.getCipherAlgoName());
+
+        return DecipherResponse.builder()
+                .decipherText(decipheredStr)
                 .build();
     }
 }
